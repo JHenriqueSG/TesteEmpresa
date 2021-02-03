6e766988ec0aa5bcc848227d19793fb6a0cffe74
José Henrique da Silva guimarães <josehenriquesg@hotmail.com>
2021-02-02 23:56:43 -0300

    Adicionar arquivos de projeto.

---
+++ TesteEmpresa/Migrations/20210202212524_Clientes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TesteEmpresa.Migrations
+{
+    public partial class Clientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Razao_Social = table.Column<string>(maxLength: 50, nullable: true),
+                    CNPJ = table.Column<string>(maxLength: 14, nullable: true),
+                    Data_Fundacao = table.Column<DateTime>(nullable: false),
+                    Capital = table.Column<decimal>(nullable: false),
+                    Quarentena = table.Column<string>(maxLength: 1, nullable: false),
+                    Status_Cliente = table.Column<string>(maxLength: 1, nullable: false),
+                    Classificacao = table.Column<string>(maxLength: 1, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
---
+++ TesteEmpresa/Models/Clientes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteEmpresa.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "Deve ser menor que 50 caracteres.")]
+        public string Razao_Social { get; set; }
+        [StringLength(14, ErrorMessage = "Deve ser menor que 14 caracteres.")]
+        public string CNPJ { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{dd-MM-yyyy:0}", ApplyFormatInEditMode = true)]
+        public DateTime Data_Fundacao { get; set; }
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        [Range(0, 9999999999999999.99)]
+        public decimal Capital { get; set; }
+        [StringLength(1)]
+        public char Quarentena { get; set; }
+        [StringLength(1)]
+        public char Status_Cliente { get; set; }
+        [StringLength(1)]
+        public string Classificacao { get; set; }
+
+    }
+}
